d5ab0a008c55e2f523df772d935e7714fe76792c
1021086660@qq.com <1021086660@qq.com>
2021-05-14 07:35:18 +0800

    5-14

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,6 +19,8 @@ Page({
       const dayNotes = [];
       const tmpArr = [];
 
+      console.log(tokens);
+
       for (let i = 0; i < tokens.length; i++) {
         const item = tokens[i];
         if ((item['type'] == 'heading' && item['depth'] == 1 && !tmpArr.length) ||
@@ -27,6 +29,7 @@ Page({
         } else if (tmpArr.length > 0) {
           dayNotes.push(this.parser(tmpArr));
           tmpArr.length = 0;
+          tmpArr.push(item);
         }
       }
 
@@ -34,21 +37,36 @@ Page({
         dayNotes.push(this.parser(tmpArr));
       }
 
+      this.processNotes(dayNotes);
+
       console.log(dayNotes);
       this.setData({
-        dayNotes,
+        dayNotes: dayNotes.reverse(),
       });
     })
   },
 
   processNotes(res) {
-    res.forEach((res) => {
-      res.greetings = '还好吗？';
+    res.forEach((item) => {
+      ['note', 'idea'].forEach((name) => {
+        const newnote = [];
+        const note = item[name] || [];
+
+        for (let i = 0; i < note.length; i++) {
+          newnote.push(...note[i].split('\n'));
+        }
+
+        item[name] = newnote;
+      })
+
     });
     return res;
   },
 
-  parser(tokens, res = {}) {
+  /**
+   * { day: '', idea: ['我的想法'], note: ['引用内容'], origin: '引用出处', url: '' }
+   */
+  parser(tokens, res = {}, ancestorType) {
 
     for (let i = 0; i < tokens.length; i++) {
       const { type, depth, text, tokens: childs, href } = tokens[i];
@@ -60,13 +78,24 @@ Page({
           }
           break;
         case 'text':
-          res['note'] = res['note'] || [];
-          res['note'].push(text);
+          if (ancestorType == 'blockquote') {
+            res['note'] = res['note'] || [];
+            res['note'].push(text);
+          } else {
+            res['idea'] = res['idea'] || [];
+            res['idea'].push(text);
+          }
           break;
         case 'blockquote':
+          this.parser(childs, res, type);
+          break;
         case 'paragraph':
         case 'em':
-          this.parser(childs, res);
+          if (ancestorType == 'blockquote') {
+            this.parser(childs, res, 'blockquote');
+          } else {
+            this.parser(childs, res, type);
+          }
           break;
         case 'link':
           res['origin'] = text;
