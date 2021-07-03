f1247f7dbc1d01d006b7d1ac49451902953b1e7f
liubei90 <1021086660@qq.com>
2021-07-03 17:39:12 +0800

    调整h5页面

--- h5/edit-rticle/src/App.vue
+++ h5/edit-rticle/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: liubei
  * @Date: 2021-06-17 16:45:54
- * @LastEditTime: 2021-06-29 15:10:57
+ * @LastEditTime: 2021-06-30 11:44:22
  * @Description: 
 -->
 <template>
@@ -9,18 +9,33 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, onUnmounted } from 'vue'
-import Vditor from 'vditor'
-import "vditor/src/assets/scss/index.scss"
+import { defineComponent, onMounted, onUnmounted } from "vue";
+import Vditor from "vditor";
+import "vditor/src/assets/scss/index.scss";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   setup: () => {
     let theVditor;
     onMounted(() => {
-      theVditor = new Vditor('vditor', {
-        width: '100%',
-        height: '100%',
+      theVditor = new Vditor("vditor", {
+        toolbarConfig: {
+          pin: true,
+        },
+        counter: {
+          enable: true,
+        },
+        height: window.innerHeight / 2,
+        toolbar: [
+          "emoji",
+          "link",
+          "upload",
+          "edit-mode",
+          {
+            name: "more",
+            toolbar: ["insert-after", "fullscreen", "preview", "info", "help"],
+          },
+        ],
       });
     });
 
@@ -28,13 +43,14 @@ export default defineComponent({
       theVditor.destroy();
     });
 
-    return {}
-  }
-})
+    return {};
+  },
+});
 </script>
 
 <style>
-html, body {
+html,
+body {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
